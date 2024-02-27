932be268a510e20ab3c1f20f2fa018f4061a0739
Rahim Nikfar <rahim.n3@gso.schule.koeln>
2024-02-27 11:52:21 +0100

    Projektdateien hinzufügen.

---
+++ Projekt-Arbeit-Rahim/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Projekt_Arbeit_Rahim
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
