0eb83572753d7453117069afd82ddeebcfd46b84
Rahim Nikfar <rahim.n3@gso.schule.koeln>
2024-03-01 10:57:07 +0100

    uPd

--- Projekt-Arbeit-Rahim/Database/DatabaseDefiner.cs
+++ Projekt-Arbeit-Rahim/Database/DatabaseDefiner.cs
@@ -1,12 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Projekt_Arbeit_Rahim.Database
-{
-    internal class Class1
-    {
-    }
-}
+
--- Projekt-Arbeit-Rahim/Program.cs
+++ Projekt-Arbeit-Rahim/Program.cs
@@ -1,10 +1,58 @@
-﻿namespace Projekt_Arbeit_Rahim
+﻿using System;
+
+namespace Projekt_Arbeit_Rahim
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            while (true)
+            {
+                int userInput;
+
+                Console.WriteLine("----------------------------------------");
+                Console.WriteLine("Willkommen zum The Binding of Isaac Wiki.");
+                Console.WriteLine("----------------------------------------");
+                Console.WriteLine("Drücken Sie eine beliebige Taste zum Fortfahren.");
+                Console.WriteLine("----------------------------------------");
+                Console.ReadKey();
+                Console.Clear();
+
+                Console.WriteLine("1. Item Namen Liste \n2. Item Info \n3. Clear \n4. Beenden");
+                Console.WriteLine("----------------------------------------");
+                Console.WriteLine("Eingabe:");
+
+
+                if (int.TryParse(Console.ReadLine(), out userInput))
+
+                {
+                    switch (userInput)
+                    {
+                        case 1:
+                            Console.WriteLine("Option 1 ausgewählt");
+                            break;
+                        case 2:
+                            Console.WriteLine("Option 2 ausgewählt");
+                            break;
+                        case 3:
+                            Console.Clear();
+                            break;
+                        case 4:
+                            Environment.Exit(0);
+                            break;
+                        default:
+                            
+                            Console.WriteLine("Ungültige Eingabe. Kein Item gefunden.\n bitte einde taste drücken zum fortfahren");
+                            Console.ReadKey();
+                            Console.Clear();
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Ungültige Eingabe. Bitte geben Sie eine Zahl ein.");
+                }
+            }
         }
     }
 }
