95b397b8afe90cfbe43187ed5544cf96ceba27bf
Rahim Nikfar <rahim.n3@gso.schule.koeln>
2024-03-19 11:56:29 +0100

    F githubh

--- Projekt-Arbeit-Rahim/Database/DatabaseDefiner.cs
+++ Projekt-Arbeit-Rahim/Database/DatabaseDefiner.cs
@@ -1,8 +1,35 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 
+namespace Projekt_Arbeit_Rahim.Database
+{
+    internal class DatabaseDefiner : DbContext
+    {
+        public DbSet<BaseItem> BaseItems { get; set; } = null!;
+        public DbSet<ModItem> ModItems { get; set; } = null!;
+       
 
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<BaseItem>().HasKey(MI => MI.Id);
+            modelBuilder.Entity<ModItem>().HasKey(MI => MI.Id);
+          
+           
+            base.OnModelCreating(modelBuilder);
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlite($@"Data Source={Directory.GetCurrentDirectory()}\Database\Database.db");
+            }
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
